7faa3e7e0135633ee108d2c6e53d84a816489311
nmallar <mallar@texellentsolutions.com>
2023-08-12 22:44:31 +0530

    modify test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -10,3 +10,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiply(self):
+        assert 100 == calculator.multiply(10, 10)
