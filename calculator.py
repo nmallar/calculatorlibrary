185e39ac9622e366276da09c24cf323eec6a6d70
nmallar <mallar@texellentsolutions.com>
2023-08-12 19:08:11 +0530

    added functions

---
+++ calculator.py
@@ -0,0 +1,8 @@
+"""Calculator library containing basic math operations"""
+
+
+def add(first_term,second_term):
+    return first_term+second_term
+
+def subtract(first_term,second_term):
+    return first_term - second_term
